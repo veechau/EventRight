e9881a7677b85e6bc1e8b12b32f4745e6ff988db
veechau <veechau@gmail.com>
2016-06-30 17:23:33 -0700

    Added Events Actions - not yet tested

--- frontend/actions/gathering_action.js
+++ frontend/actions/gathering_action.js
@@ -7,5 +7,59 @@ const ErrorActions = require('./error_actions');
 const hashHistory = require('react-router').hashHistory;
 
 const GatheringActions = {
+
+  // Client-side
+
+  fetchGatherings(){
+    GatheringApiUtil.fetchAllGatherings(
+      GatheringActions.receiveAll,
+      ErrorActions.setErrors);
+  },
+
+  getGathering(){
+    GatheringApiUtil.getGathering(
+      GatheringActions.receiveGathering,
+      ErrorActions.setErrors);
+  },
+
+  createGathering(){
+    GatheringApiUtil.createGathering(
+      GatheringActions.receiveGathering,
+      ErrorActions.setErrors);
+  },
+
+  editGathering(){
+    GatheringApiUtil.updateGathering(
+      GatheringActions.receiveGathering,
+      ErrorActions.setErrors);
+  },
+
+  deleteGathering(){
+    GatheringApiUtil.deleteGathering(
+      GatheringActions.removeGathering,
+      ErrorActions.setErrors);
+  },
+
+  // Server-side
+  receiveAll(gatherings){
+    AppDispatcher.dispatch({
+      actionType: GatheringConstants.EVENTS_RECEIVED,
+      gatherings: gatherings
+    });
+  },
+  receiveGathering(gathering){
+    AppDispatcher.dispatch({
+      actionType: GatheringConstants.EVENT_RECEIVED,
+      gathering: gathering
+    });
+  },
+  removeGathering(gathering){
+    AppDispatcher.dispatch({
+      actionType: GatheringConstants.EVENT_REMOVED,
+      gathering: gathering
+    });
+  }
+
 };
+
 module.exports = GatheringActions;
