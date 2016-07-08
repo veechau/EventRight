788b3de2cfd8463895bcacd71864a98c073c8b33
veechau <veechau@gmail.com>
2016-07-07 21:23:45 -0700

    Change header text

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -35493,52 +35493,55 @@
 	    return React.createElement(
 	      Carousel,
 	      {
+	        className: 'app-slider-div',
 	        dragging: true,
 	        slidesToShow: 1,
 	        speed: 200,
-	        autoplay: true
+	        autoplay: true,
+	        frameOverflow: 'hidden',
+	        initialSlideHeight: 0.7
 	      },
 	      React.createElement(
 	        'div',
 	        null,
 	        React.createElement('img', { src: 'https://res.cloudinary.com/vechau/image/upload/c_scale,h_1600/v1467271000/AppSlider/photo-1453090927415-5f45085b65c0_o2iojc.jpg',
-	          onLoad: this.handleImageLoaded.bind(this),
-	          onError: this.handleImageErrored.bind(this) })
+	          onLoad: this.handleImageLoaded,
+	          onError: this.handleImageErrored })
 	      ),
 	      React.createElement(
 	        'div',
 	        null,
 	        React.createElement('img', { src: 'https://res.cloudinary.com/vechau/image/upload/c_scale,h_1600/v1467272765/AppSlider/photo-1445965752525-ac2d3c195ffe_qdcaum.jpg',
-	          onLoad: this.handleImageLoaded.bind(this),
-	          onError: this.handleImageErrored.bind(this) })
+	          onLoad: this.handleImageLoaded,
+	          onError: this.handleImageErrored })
 	      ),
 	      React.createElement(
 	        'div',
 	        null,
 	        React.createElement('img', { src: 'https://res.cloudinary.com/vechau/image/upload/v1467272522/AppSlider/photo-1450859018738-29f67b1a6102_mmvfxu.jpg',
-	          onLoad: this.handleImageLoaded.bind(this),
-	          onError: this.handleImageErrored.bind(this) })
+	          onLoad: this.handleImageLoaded,
+	          onError: this.handleImageErrored })
 	      ),
 	      React.createElement(
 	        'div',
 	        null,
 	        React.createElement('img', { src: 'https://res.cloudinary.com/vechau/image/upload/c_scale,h_1600/v1467272182/AppSlider/photo-1415886541506-6efc5e4b1786_fmkcyh.jpg',
-	          onLoad: this.handleImageLoaded.bind(this),
-	          onError: this.handleImageErrored.bind(this) })
+	          onLoad: this.handleImageLoaded,
+	          onError: this.handleImageErrored })
 	      ),
 	      React.createElement(
 	        'div',
 	        null,
 	        React.createElement('img', { src: 'https://res.cloudinary.com/vechau/image/upload/v1467270960/AppSlider/concert_lwmxi5.jpg',
-	          onLoad: this.handleImageLoaded.bind(this),
-	          onError: this.handleImageErrored.bind(this) })
+	          onLoad: this.handleImageLoaded,
+	          onError: this.handleImageErrored })
 	      ),
 	      React.createElement(
 	        'div',
 	        null,
 	        React.createElement('img', { src: 'https://res.cloudinary.com/vechau/image/upload/c_scale,h_1600/v1467272192/AppSlider/photo-1459749411175-04bf5292ceea_zm9a2g.jpg',
-	          onLoad: this.handleImageLoaded.bind(this),
-	          onError: this.handleImageErrored.bind(this) })
+	          onLoad: this.handleImageLoaded,
+	          onError: this.handleImageErrored })
 	      )
 	    );
 	  }
